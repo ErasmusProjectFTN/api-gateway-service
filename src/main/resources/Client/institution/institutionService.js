d7aa9948a76ba87f1b03ddd9b925c53928e701c0
Nebojsa <soneneso93@gmail.com>
2017-03-07 21:27:11 +0100

    Added Institution service and finished form

---
+++ src/main/resources/Client/institution/institutionService.js
@@ -0,0 +1,21 @@
+angular.module('wsapp')
+.service('institutionService', function($http){
+	return{
+		insertInstitution: function(onSuccess, onError){ // TODO Add parameters here
+		
+		var req = {
+		    method: 'POST',
+		    url: '//institution/add', // here to write a appropriate api
+		    headers: {
+		        'Content-Type': 'application/x-www-form-urlencoded'
+		    },
+		    data: $.param({ 
+		    	name: name
+		    })
+		}	
+
+		$http(req).then(onSuccess, onError);
+		
+		}
+	}
+});
