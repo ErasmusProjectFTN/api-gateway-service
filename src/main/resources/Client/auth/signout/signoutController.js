068c0802d73a9b33502bc5459993159a6aeb7781
Nina <nina.marjanovic@gmail.com>
2017-08-23 16:35:12 +0200

    signout

--- src/main/resources/Client/auth/signout/signoutController.js
+++ src/main/resources/Client/auth/signout/signoutController.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('wsapp')
-    .controller('SignoutController', function ($scope, authService, $location, $state) {
+    .controller('SignoutController', function ($scope, authService, $location, $state, $cookies) {
 
-
+    	$cookies.remove('loginFlag')
+		$cookies.remove('student')
+		$state.go('program')
+    	
 
     });
